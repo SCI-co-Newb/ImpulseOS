510449531a46fd0008a966ada1a17d7d6620cc14
Sahajdeep Singh <impulseVD@hotmail.com>
2023-10-25 13:06:00 -0400

    Says Hello and updated README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ImpulseOS
+
+On a mac, install Homebrew and then 'brew install qemu nasm'
+
+To compile: nasm -f bin boot_sect.asm -o boot_sect.bin
+
+To run: qemu-system-x86_64 boot_sect.bin -nographic
+
+(you may not need -nographic or -system-x86_64)
--- boot_sect.asm
+++ boot_sect.asm
@@ -1,6 +1,16 @@
-; Infinite Loop
-loop:
-    jmp loop
+mov ah, 0x0e ; tty (teletypewriter) mode
+mov al, 'H' ; al is the lower part of ax
+int 0x10 ; this is an interrupt, this specifically is a general interrupt for video services
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10 ; since 'l' is still on al, I assume mov copies 'l' into al each interrupt continously
+mov al, 'o'
+int 0x10
+
+jmp $ ; jump to current address = infinite loop
+
 
 ; Fill with 510 zeros so AA55 goes to 511-512
 times 510-($-$$) db 0
